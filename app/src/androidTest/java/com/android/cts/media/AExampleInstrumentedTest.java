06f24e6dcde91b48e00f5b17d7470214407d1516
李国显 <guoxian.li@douzhibo.com>
2018-04-20 15:46:37 +0800

    first commit

---
+++ app/src/androidTest/java/com/android/cts/media/AExampleInstrumentedTest.java
@@ -0,0 +1,30 @@
+package com.android.cts.media;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class AExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.android.cts.media", appContext.getPackageName());
+    }
+
+    public Context getContext(){
+        return InstrumentationRegistry.getTargetContext();
+    }
+}
